3b4e0fcc4a594e6097967b149ad03216f8e9d3e1
Madeleine Lorås <madeleineloras@Madeleines-MacBook-Air.local>
2024-09-16 10:28:04 +0200

    Add live demo code 16.09

--- Felleskoding/2024/script.js
+++ Felleskoding/2024/script.js
@@ -1,10 +1,38 @@
-console.log("YOLO");
+// console.log("YOLO");
 
-// Oppgave 1
-//Lag en variabel som inneholder strengen «Hello World!»
-//og skriv den ut til konsollen.
+// // Oppgave 1
+// //Lag en variabel som inneholder strengen «Hello World!»
+// //og skriv den ut til konsollen.
 
-// Oppgave 2
-// Lag en variabel med navnet ditt.
-// Skriv ut «Hallo (ditt navn)» ved å kombinere
-// «Hallo» med ditt navn.
+// // Oppgave 2
+// // Lag en variabel med navnet ditt.
+// // Skriv ut «Hallo (ditt navn)» ved å kombinere
+// // «Hallo» med ditt navn.
+
+// let yesNoForm = document.createElement("form")
+
+// let inputYes = document.createElement("input")
+// inputYes.type = "radio" 
+// inputYes.id = "radioeYes"
+// inputYes.value = "yes"
+
+// let inputYesLabel = document.createElement("label")
+// inputYesLabel.for = "radioYes"
+// inputYesLabel.innerHTML = "Ja"
+
+// yesNoForm.appendChild(inputYesLabel)
+// yesNoForm.appendChild(inputYes)
+
+// document.body.appendChild(yesNoForm)
+
+
+
+function confirmButton(){
+    let choiceYes = document.getElementById("radioYes").checked;
+    let choiceNo = document.getElementById("radioNo").checked;
+
+    if (choiceNo) {
+        alert("Artig vits")
+    }
+    
+}
