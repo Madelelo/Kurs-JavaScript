56325a969597b8265ce6b8afd9cc2a343e30fd0a
Madeleine Lorås <madeleineloras@Madeleines-MacBook-Air.local>
2024-09-17 14:21:38 +0200

    Update live demo 17.09

--- Felleskoding/2024/script.js
+++ Felleskoding/2024/script.js
@@ -1,13 +1,3 @@
-// console.log("YOLO");
-
-// // Oppgave 1
-// //Lag en variabel som inneholder strengen «Hello World!»
-// //og skriv den ut til konsollen.
-
-// // Oppgave 2
-// // Lag en variabel med navnet ditt.
-// // Skriv ut «Hallo (ditt navn)» ved å kombinere
-// // «Hallo» med ditt navn.
 
 let yourForm = document.createElement("form")
 
@@ -35,3 +25,167 @@ function confirmButton(){
     }
     
 }
+  
+
+let pokemon = { 
+
+    pikachu: { 
+    
+      name: "Pikachu", 
+    
+      type: "Electric", 
+    
+      abilities: ["Static", "Lightning Rod"], 
+    
+      healthPoints: 35, 
+    
+      attackPoints: 55 
+    
+    }, 
+    
+    charmander: { 
+    
+      name: "Charmander", 
+    
+      type: "Fire", 
+    
+      abilities: ["Blaze", "Solar Power"], 
+    
+      healthPoints: 39, 
+    
+      attackPoints: 52 
+    
+    }, 
+    
+    squirtle: { 
+    
+      name: "Squirtle", 
+    
+      type: "Water", 
+    
+      abilities: ["Torrent", "Rain Dish"], 
+    
+      healthPoints: 44, 
+    
+      attackPoints: 48 
+    
+    }, 
+    
+    bulbasaur: { 
+    
+      name: "Bulbasaur", 
+    
+      type: "Grass/Poison", 
+    
+      abilities: ["Overgrow", "Chlorophyll"], 
+    
+      healthPoints: 45, 
+    
+      attackPoints: 49 
+    
+    }, 
+    
+    jigglypuff: { 
+    
+      name: "Jigglypuff", 
+    
+      type: "Fairy/Normal", 
+    
+      abilities: ["Cute Charm", "Friend Guard"], 
+    
+      healthPoints: 115, 
+    
+      attackPoints: 45 
+    
+    }, 
+    
+    raichu: { 
+    
+      name: "Raichu", 
+    
+      type: "Electric", 
+    
+      abilities: ["Static", "Lightning Rod"], 
+    
+      healthPoints: 60, 
+    
+      attackPoints: 90 
+    
+    }, 
+    
+    blastoise: { 
+    
+      name: "Blastoise", 
+    
+      type: "Water", 
+    
+      abilities: ["Torrent", "Rain Dish"], 
+    
+      healthPoints: 79, 
+    
+      attackPoints: 83 
+    
+    }, 
+    
+    venusaur: { 
+    
+      name: "Venusaur", 
+    
+      type: "Grass/Poison", 
+    
+      abilities: ["Overgrow", "Chlorophyll"], 
+    
+      healthPoints: 80, 
+    
+      attackPoints: 82 
+    
+    }, 
+    
+    charizard: { 
+    
+      name: "Charizard", 
+    
+      type: "Fire/Flying", 
+    
+      abilities: ["Blaze", "Solar Power"], 
+    
+      healthPoints: 78, 
+    
+      attackPoints: 84 
+    
+    }, 
+    
+    clefairy: { 
+    
+      name: "Clefairy", 
+    
+      type: "Fairy", 
+    
+      abilities: ["Cute Charm", "Magic Guard"], 
+    
+      healthPoints: 70, 
+    
+      attackPoints: 45 
+    
+    } 
+    
+    }; 
+ console.log(pokemon) 
+
+ let pokemonHeading = document.createElement("h1")
+ pokemonHeading.innerHTML = "Electric pokemons"
+ pokemonHeading.style.color = "yellow"
+ document.getElementById("pokemon").appendChild(pokemonHeading)
+ 
+
+ for (const p in pokemon) {
+    let pokemonList = document.createElement("p")
+
+    if (pokemon[p].type =="Electric") {
+        pokemonList.innerHTML = pokemon[p].name +pokemon[p].healthPoints
+        document.getElementById("pokemon").appendChild(pokemonList)
+    }
+
+
+    
+ }
