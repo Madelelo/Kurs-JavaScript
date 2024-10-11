41af2f7bab429cf2622891ea8343f0406acbe034
Madeleine Lorås <madeleineloras@Madeleines-Air.home>
2024-10-11 09:52:33 +0200

    Løsninger for Funksjoner oppgaver

--- JavaScript3/script.js
+++ JavaScript3/script.js
@@ -1,3 +1,52 @@
+//Oppgave 1
+function printMelding(navn){
+    console.log("hello ", navn)
+}
+
+printMelding("Madeleine") // kaller funksjonen
+
+//Oppgave 2
+function kastTerning(antallSider){
+    return Math.floor(Math.random()*(antallSider+1))
+}
+console.log(kastTerning(6))
+
+//Oppgave 3
+function generererHisotrie(navn, adjektiv1, adjektiv2, adjektiv3, adjektiv4){
+    return `Det var en gang en ${adjektiv1} IM-elev som het ${navn}. I det ${adjektiv2} JavaScript-kurset lærte ${navn} om variabler og funksjoner sammen med ${adjektiv3} klassekamerater. Læreren i kurset er veldig ${adjektiv4}, noe som gjør kursdagene ekstra spennende. `
+}
+
+console.log(generererHisotrie("Madde", "glitrende", "morsomme", "skummle","fabelaktive"))
+
+//Oppgave 4
+
+
+
+const printMessage = (nameOfPerson) => { return `Hello ${nameOfPerson}`}
+
+console.log(printMessage("Madde"))
+
+//Oppgave 6
+function erPalindrom(ord){
+    let counter =0; // bruker en counter for å telle hvor mange bokstaver som er like
+
+    for (let bokstavIndex = 0; bokstavIndex < ord.length; bokstavIndex++) {
+        if (ord[bokstavIndex] == ord[ord.length-1-bokstavIndex]) {
+        counter++ // hvis første og siste bokstav er lik, pluss en
+        }
+    }
+if (counter == ord.length) {
+    return true;
+}else {
+    return false;
+}
+
+}
+
+console.log(erPalindrom("level"))
+
+
+
 
 function skrivMelding(){
    let melding = "Heihei"
@@ -6,9 +55,9 @@ function skrivMelding(){
 
 console.log(skrivMelding())
 
-minFunksjon(argument) // Printer argument
+//minFunksjon(argument) // Printer argument
 
-minFunksjon() //Printer yolo til konsollen
+//minFunksjon() //Printer yolo til konsollen
 
 function klikkMeg(){
     console.log("Klikk!")
